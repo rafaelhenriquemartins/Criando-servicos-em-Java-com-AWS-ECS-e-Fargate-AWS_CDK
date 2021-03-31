bee1152e56c818bab3711029e2b34b6b3eecfe51
Rafael Martins <rafaelrhm.27@gmail.com>
2021-03-31 15:13:16 -0300

    Consumindo eventos de produtos com SQS

---
+++ src/main/java/com/myorg/RdsStack.java
@@ -0,0 +1,56 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.*;
+import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.rds.*;
+
+import java.util.Collections;
+
+public class RdsStack extends Stack {
+    public RdsStack(final Construct scope, final String id, Vpc vpc) {
+        this(scope, id, null, vpc);
+    }
+
+    public RdsStack(final Construct scope, final String id, final StackProps props, Vpc vpc) {
+        super(scope, id, props);
+
+        CfnParameter databasePassword = CfnParameter.Builder.create(this, "databasePassword")
+                .type("String")
+                .description("The RDS instance password")
+                .build();
+
+        ISecurityGroup iSecurityGroup = SecurityGroup.fromSecurityGroupId(this, id, vpc.getVpcDefaultSecurityGroup());//busco o security group padroa dessa vpc
+        iSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3306));// add nova regra de entrada, para qualquer Ip da internet pode ter acesso atraves do protocolo TCP.
+
+        DatabaseInstance databaseInstance = DatabaseInstance.Builder
+                .create(this, "Rds01")
+                .instanceIdentifier("aws-project01-db")
+                .engine(DatabaseInstanceEngine.mysql(MySqlInstanceEngineProps.builder()
+                        .version(MysqlEngineVersion.VER_5_6)
+                        .build()))
+                .vpc(vpc)//aqui que digo que sera criada dentro da minha vpc
+                .credentials(Credentials.fromUsername("admin",
+                        CredentialsFromUsernameOptions.builder()
+                                .password(SecretValue.plainText(databasePassword.getValueAsString()))
+                                .build()))
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .multiAz(false)
+                .allocatedStorage(10)
+                .securityGroups(Collections.singletonList(iSecurityGroup))
+                .vpcSubnets(SubnetSelection.builder()
+                        .subnets(vpc.getPrivateSubnets())
+                        .build())
+                .build();
+
+        CfnOutput.Builder.create(this, "rds-endpoint")
+                .exportName("rds-endpoint")
+                .value(databaseInstance.getDbInstanceEndpointAddress())
+                .build();
+
+        CfnOutput.Builder.create(this, "rds-password")
+                .exportName("rds-password")
+                .value(databasePassword.getValueAsString())
+                .build();
+
+    }
+}
